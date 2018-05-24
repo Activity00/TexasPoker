71a87e5cbeec53a7a3bf24138aefce8725731ced
Activity00 <1032662429@qq.com>
2018-05-24 20:13:10 +0800

    ADD: time beat heat

--- server/game/initialization.py
+++ server/game/initialization.py
@@ -4,11 +4,22 @@
 @author: 武明辉 
 @time: 2018/4/25 10:48
 """
+from tornado import ioloop
+
 from scat import ScatObject
 from scat.utils.token_manager import TokenManager
 from texas import settings
 
 ScatObject.web_root.token_mgr = TokenManager(settings.TOKEN_PRI_KEY)
+
+
+def register_server_info():
+    server_info = {'name': 'xxoo'}
+    dd = ScatObject.remote['hall'].call_remote("register_server", server_info)
+
+
+ioloop.PeriodicCallback(register_server_info, 1000 * 10).start()
+
 
 if __name__ == '__main__':
     pass
--- server/hall/service/rootservice.py
+++ server/hall/service/rootservice.py
@@ -5,13 +5,19 @@
 @time: 2018/2/10 19:38
 """
 
+from scat import ScatObject
+from scat.service import root_service
 
-def rootservice(aaa):
+
+@root_service
+def hert_beat(self, server_info):
+    """ 接受游戏服务器信息 """
     pass
 
 
-@rootservice
-class HallRootService:
-    def hert_beat(self, server_info):
-        """ 接受游戏服务器信息 """
-        pass
+@root_service
+def register_server(server_info):
+    """ 接受游戏服务器信息 """
+    mapping = ScatObject.web_root.room_server_mapping = {}  # name: {ip, port, load}
+    mapping.update(server_info)
+
